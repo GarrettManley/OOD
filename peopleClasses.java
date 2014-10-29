063254a4a745fb2b7279d5b4f15a0d24dbaf13e1
CarlyPhenis <cp02686@georgiasouthern.edu>
2014-10-28 21:00:01 -0400

    Update peopleClasses.java

--- peopleClasses.java
+++ peopleClasses.java
@@ -7,12 +7,17 @@
 class Person {
 	Card[] hand = new Card[12]; //size 12 because 11 is the most you could have without busting
 	int cards = 0;
+	int total = 0; //Used to hold the total value of the cards drawn so far.
 	
 	public void hit() {
 		hand[cards] = deck.drawCard();
+		total =+ hand[cards].getValue();
 		cards++;
 	}
-	
+	//Place holder for the stand method.
+	public void stand(){
+		
+	}
 }
 
 class Player extends Person {
@@ -25,11 +30,22 @@ class Player extends Person {
 
 class Dealer extends Person {
 	//dealer will not have to worry about money
+	//Decides whether the dealer will hit or stand, depending on the value of the cards.
+	public void play(){
+		if(total < 17)
+			hit();
+		else
+			stand();
+	}
 }
 
 //card and deck classes just for place holders
 class Card {
+	int value;
 	
+	public int getValue(){
+		return value;
+	}	
 }
 
 class deck {
