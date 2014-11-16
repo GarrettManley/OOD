7ef37b276a056dda6b26fc937ed1523b674d7559
GarrettManley <GarrettManley@gmail.com>
2014-11-16 18:14:50 -0500

    Create CardTemplate.java

---
+++ CardTemplate.java
@@ -0,0 +1,12 @@
+
+public abstract class CardTemplate {
+	public final void makeCard(int v){
+		assignSuite();
+		assignValue(v);
+	}
+	public abstract void assignSuite();
+	public abstract void assignValue(int v);
+	public abstract int getNumValue();
+	public abstract String getValue();
+	public abstract String getSuite();
+}
