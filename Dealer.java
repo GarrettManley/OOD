68717b37509247f8635e51706c7b73562944c9d3
GarrettManley <GarrettManley@gmail.com>
2014-11-18 16:52:41 -0500

    Update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -1,25 +1,22 @@
 
 class Dealer extends Person {
-	Deck d1 = new Deck();
-	int deckPlace = 0;
-	//dealer will not have to worry about money
-	//Deals out two cards to each player, alternating which is being dealt the card so that they aren't given the same card.
-	public void deal(Player p){
-		for(int i = 0; i < 4; i++){
-			if(i == 0 || i == 2)
-			p.hit(dealHit());
-			else
-			this.hit(dealHit());
-		}		
+	
+	private Deck deck = new Deck();
+	private int deckPos = 0;
+	
+	Dealer(){
+		this.personType = 2;
+		this.hand.addObserver(bo);
 	}
+	
+	public void deal(Person p){
+		p.hand.addCard(deck.shuffDeck.get(this.deckPos));
+		this.deckPos++;
+	}
+	
 	//Dealer will hit if the total is less than 17 and stand if it isn't. Also checks hand against the player's final hand.
 	public void play(Player p){
-		while(total < 17 && total < p.getTotal())
-			hit(dealHit());
+		
 	}
-	public CardTemplate dealHit(){
-		CardTemplate ct = d1.shuffDeck.get(deckPlace);
-		deckPlace++;
-		return ct;
-	}
+	
 }
