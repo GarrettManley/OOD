3acd32d83bf28086550f77b4a9b2e6d63e407b70
GarrettManley <GarrettManley@gmail.com>
2014-11-20 01:14:57 -0500

    Update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -1,33 +1,27 @@
-//add to github
 
-import java.util.Observable;
-import java.util.Observer;
-
-public class BustObserver implements Observer {
-
-	BustObserver() {
+class Dealer extends Person {
+	
+	private Deck deck = new Deck();
+	private int deckPos = 0;
+	
+	Dealer(){
+		this.personType = 2;
+		this.hand.addObserver(bo);
 	}
-
-	@Override
-	public void update(Observable obj, Object arg) {
-		Hand hand = (Hand) obj;
-		if (hand.hasAce = false) {
-			if (hand.getHandValue() > 21) {
-				hand.isBust = true;
-			}
-		} else {
-			if (hand.getHandValue() > 21) {
-				hand.isBust = true;
-				int count = 1;
-				for (int i = 0; i < hand.aceIndex.size(); i++) {
-					if (hand.getHandValue() - (10 * count) < 21) {
-						hand.isBust = false;
-						break;
-					}
-				}
-			}
+	
+	public void deal(Person p){
+		p.hand.addCard(deck.shuffDeck.get(this.deckPos));
+		this.deckPos++;
+	}
+	
+	//Dealer will hit if the total is less than 17 and stand if it isn't. Also checks hand against the player's final hand.
+	public void play(){
+		while(this.hand.getHandValue() < 17/* && this.hand.hasAce==false*/){
+			this.deal(this);
 		}
-
+		/*if(this.hand.hasAce==true){
+			
+		}*/
 	}
-
+	
 }
