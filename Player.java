45ddf87b5da50d1ff62d14a67ec2ddcf62c03dbb
GarrettManley <GarrettManley@gmail.com>
2014-11-27 18:07:34 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,6 +3,7 @@ import javax.swing.JOptionPane;
 class Player extends Person {
 	protected int money;
 	protected int bet;
+	protected boolean validBet = true;
 
 	public Player(int m) {
 		this.hand.addObserver(bo);
@@ -10,22 +11,48 @@ class Player extends Person {
 		this.personType = 1;
 	}
 
-	public void bet(int b) {
-		this.bet = b;
-		while (this.bet > this.money) {
-			this.bet = Integer.parseInt(JOptionPane.showInputDialog(null,
-					"Please enter a bet amount less than $" + this.money,
-					"Invalid Bet", 0));
+	public void bet(String b) {
+		try {
+			this.bet = Integer.parseInt(b);
+		} catch (Exception e) {
+			this.validBet = false;
 		}
+
+		while (this.money == 0) {
+			JOptionPane
+					.showMessageDialog(null,
+							"Cannot bet, Out of money!\nRestart Program to play again!");
+		}
+
+		if (this.bet > this.money || this.bet <= 0) {
+			this.validBet = false;
+		}
+
+		while (!this.validBet) {
+			try {
+				this.bet = Integer.parseInt(JOptionPane.showInputDialog(null,
+						"Please enter a positive integer bet amount that is less than $"
+								+ this.money, "Invalid Bet", 0));
+				if (this.bet > this.money || this.bet <= 0) {
+					this.validBet = false;
+				} else {
+					this.validBet = true;
+				}
+			} catch (Exception e) {
+				this.validBet = false;
+			}
+		}
+
+		this.validBet = true;
 		this.money -= this.bet;
 	}
 
 	public void lose() {
-		JOptionPane.showMessageDialog(null,"You Lose!");
+		JOptionPane.showMessageDialog(null, "You Lose!");
 	}
 
 	public void win() {
-		this.money += (2*this.bet);
+		this.money += (2 * this.bet);
 		JOptionPane.showMessageDialog(null, "You Win!");
 	}
 
