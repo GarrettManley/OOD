981931767ca90e9ab2d52805c99ae361ba173541
CarlyPhenis <cp02686@georgiasouthern.edu>
2014-12-04 16:10:51 -0500

    Update Player.java
    added comments

--- BJ 3.2/src/Player.java
+++ BJ 3.2/src/Player.java
@@ -1,23 +1,24 @@
 
 import javax.swing.JOptionPane;
-
+//Player class represents the user and it's used mainly for betting and displaying if the player wins or loses.
 class Player extends Person {
+	//Variables used for betting.
 	protected int money;
 	protected int bet;
 	protected boolean validBet = true;
-
+	//Constructor for Player that takes an int which is their amount of money. It also adds a bustObserver to this hand.
 	public Player(int m) {
 		this.hand.addObserver(bo);
 		this.money = m;
 	}
-
+	//Method that provides error checking for betting and removes the bet amount from the money.
 	public void bet(String b) {
 		try {
 			this.bet = Integer.parseInt(b);
 		} catch (Exception e) {
 			this.validBet = false;
 		}
-
+		
 		while (this.money == 0) {
 			JOptionPane
 					.showMessageDialog(null,
@@ -46,11 +47,11 @@ class Player extends Person {
 		this.validBet = true;
 		this.money -= this.bet;
 	}
-
+	//Displays a message if the player loses.
 	public void lose() {
 		JOptionPane.showMessageDialog(null, "You Lose!");
 	}
-
+	//Displays a message if the player wins and multiplies their bet by 2 and adds it to their money.
 	public void win() {
 		this.money += (2 * this.bet);
 		JOptionPane.showMessageDialog(null, "You Win!");
